e5be98393546509ae699a33693308d2c1cc0d1f6
Tornike Tsagareishvili <dht5152@gmail.com>
2025-01-23 23:55:33 +0400

    Add: Grid Item Style

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import carImage from "../../public/Images/1690714823637.jpg";
+import Link from "next/link";
+
 import "@/style/components/_gridItem.scss";
 
 export default function GridItem() {
-  return <div className="grid-item"></div>;
+  return (
+    <div className="grid-item">
+      <figure className="car-image-wrapper">
+        <Image src={carImage} alt="Car Image" />
+      </figure>
+      <div className="car-info-wrapper">
+        <p className="car-title">title</p>
+        <p className="price">price</p>
+        <button className="car-link-button">Show Car</button>
+      </div>
+    </div>
+  );
 }
